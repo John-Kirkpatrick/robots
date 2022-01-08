b28759cf35a2850f5100ead7de06d01eccb72796
John Kirkpatrick <jkirkpatrick@greenmountaintechnology.com>
2022-01-07 19:00:37 -0600

    cleanup

--- src/Robots.Domain/IValidatable.cs
+++ src/Robots.Domain/IValidatable.cs
@@ -2,15 +2,8 @@
 
 namespace Robots.Domain
 {
-  /// <summary>
-  ///   Interface for models to upload to IAP
-  /// </summary>
   public interface IValidatable
   {
-    /// <summary>
-    ///   returns the <see cref="ValidationResult" /> from model's validator
-    /// </summary>
-    /// <returns></returns>
     ValidationResult GetValidationResult();
   }
 }
